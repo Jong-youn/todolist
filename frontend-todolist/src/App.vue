5a25f242f62195ae3c8d851b7dd421a51ffa8ef2
ljy6816@gmail.com <ljy6816@gmail.com>
2022-03-22 17:48:06 +0900

    styling

--- frontend-todolist/src/App.vue
+++ frontend-todolist/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>TODO it!</h1>
+    <to-do-input></to-do-input>
+    <to-do-list></to-do-list>
+    <to-do-footer></to-do-footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoInput from './components/ToDoInput.vue'
+import ToDoFooter from './components/ToDoFooter.vue'
+import ToDoList from './components/ToDoList.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    ToDoInput,
+    ToDoFooter,
+    ToDoList
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #F6F6F6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
 }
 </style>
