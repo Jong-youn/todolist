06e2724e4c0ef74223faeaa91e2e6a95a0d86518
ljy6816@gmail.com <ljy6816@gmail.com>
2022-03-30 23:33:58 +0900

    Add todo crud api

---
+++ backend/src/main/java/todolist/backend/persistence/TodoRepository.java
@@ -0,0 +1,17 @@
+package todolist.backend.persistence;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import todolist.backend.model.TodoEntity;
+
+import java.util.List;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+
+    List<TodoEntity> findByUserId(String userId);
+
+
+
+}
